c9316f02d51212609d8ae0eea54c6ca46bb2a457
abenbrahim94 <abenbrahim94@gmail.com>
2015-02-11 20:59:09 -0500

    Completed Menu Layout

--- Assigmnent 3 . Ahmed Benbrahim/Program.cs
+++ Assigmnent 3 . Ahmed Benbrahim/Program.cs
@@ -10,7 +10,39 @@ namespace Assigmnent_3.Ahmed_Benbrahim
     {
         static void Main(string[] args)
         {
+            int MenuChoices;
+            string Menu;
+            bool answer;
 
+            Console.WriteLine("====================MENU====================");
+            Console.WriteLine("From the Menu below, Choose what you would like to eat ");
+            Console.WriteLine("Please Enter your Choice: ");
+            Console.WriteLine("====================================");
+            Console.WriteLine("1 - Burger & Fries ");
+            Console.WriteLine("2 - Pizza ");
+            Console.WriteLine("3 - Garden Salad ");
+            Console.WriteLine("4 - Wings & Fries ");
+            Console.WriteLine("5 -  Greek Salad");
+            Console.WriteLine("6 - Falafel ");
+            Console.WriteLine("7 - Spaghetti & Meatballs ");
+            Console.WriteLine("8 - Fried Chicken & Fries ");
+            Console.WriteLine("9 - Rice & Curry ");
+            Console.WriteLine("10 - Shawarma ");
+            Console.WriteLine("Press 11 to Exit ");
+
+
+            Console.WriteLine("Please make your selection (1 to 10) And 11 to Exit");
+
+            Console.WriteLine("Please Enter your Choice: ");
+    
+            Menu = Console.ReadLine();
+            Console.Write("\n");
+            MenuChoices = Convert.ToInt32(Menu);
+
+
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("press any key to exit");
+            Console.ReadKey();
 
 
 
